7e36c4d712cd8e6d6217ece65ab42b0a839bdc90
Worldwidebrine <40757969+Worldwidebrine@users.noreply.github.com>
2023-05-02 00:05:12 +0800

    Update create-promise-callback.js

--- create-promise-callback.js
+++ create-promise-callback.js
@@ -1,6 +1,39 @@
-const resolved = Promise.resolve();
-export function createPromiseByCallback(passResult) {
-    const promise = new Promise((...args) => {
+var resolved = Promise.resolve();
+export function createPromiseAsync(passResult) {
+    var promise = new Promise(function (...args) {
         resolved.then(() => passResult(promise, ...args)); // queueMicrotask to make sure promise has been initialized
     });
-}
+};
+export function createPromiseSync(passResult) {
+    var arg = null;
+    var promise = new Promise(function (...args) {
+        arg = args;
+    });
+    passResult(promise, ...arg);
+};
+export function createPromisePromised() {
+    return new Promise(function (resolve) {
+        var promise = new Promise(function (broadcastFulfillment, broadcastRejection) {
+            resolve(resolved.then(function () {
+                return {
+                    promise,
+                    resolve: broadcastFulfillment,
+                    reject: broadcastRejection
+                };
+            }));
+        });
+    });
+};
+export function createPromise() {
+    var res = null;
+    var rej = null;
+    var promise = new Promise(function (broadcastFulfillment, broadcastRejection) {
+        res = broadcastFulfillment;
+        rej = broadcastRejection;
+    });
+    return {
+        promise,
+        resolve: res,
+        reject: rej
+    };
+};
