342de3cfddc337a5617712d7505128fc98aab77a
Worldwidebrine <40757969+Worldwidebrine@users.noreply.github.com>
2023-05-02 10:51:22 +0800

    Update create-native-promise.js

--- create-native-promise.js
+++ create-native-promise.js
@@ -1,21 +1,16 @@
-class ResolvedThenable {
-    then = ((broadcastFulfillment) => {
-        broadcastFulfillment();
-    });
-}
 export const createNativePromise = async () => {
-    let res = null;
-    let rej = null;
-    const promise = (async () => ({
-        then: (broadcastFulfillment, broadcastRejection) => {
-            res = broadcastFulfillment;
-            rej = broadcastRejection;
+    return await {
+        then: (resolve) => {
+            const promise = (async () => ({
+                then: async (broadcastFulfillment, broadcastRejection) => {
+                    await null;
+                    resolve({
+                        promise,
+                        resolve: broadcastFulfillment,
+                        reject: broadcastRejection
+                    });
+                }
+            }))();
         }
-    }))();
-    await new ResolvedThenable; // await another thenable object to make sure the promise has been constructed
-    return {
-        promise,
-        resolve: res,
-        reject: rej
     };
 };
